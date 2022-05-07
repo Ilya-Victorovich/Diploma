d734f8cb810aabf041f46a8584ff095be8ac7637
Ilya Voroshin <Iliya-Voroshin-2000@ya.ru>
2022-05-07 20:55:35 +0700

    addition trials test 1

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user
 from werkzeug.security import check_password_hash, generate_password_hash
+import rpy2.robjects as robjects
 
 AUTHORIZED_LOGIN = None  # Значение логина, под которым зашел пользователь
 
@@ -22,14 +23,18 @@ def main():
         id = db.Column(db.Integer, primary_key=True)
         login = db.Column(db.String(20), nullable=False, unique=True)
         password = db.Column(db.String(30), nullable=False)
-        tr = db.relationship('Trials', backref='Users', uselist=False)
+        tr = db.relationship('Trials', backref='Users', uselist=True)
 
     class Trials(db.Model):
         id = db.Column(db.Integer, primary_key=True)
         username = db.Column(db.String(20))
         title = db.Column(db.String(20))
+
         date = db.Column(db.DateTime, default=datetime.utcnow)
-        '''связь таблицы пользователя и исследований - один к многим, значение таблицы в ForeignKey - с маленькой 
+        number_of_participants = db.Column(db.Integer)  # количество испытуемых
+        number_of_interventions = db.Column(db.Integer)  # количество вмешательств
+
+        '''связь таблицы пользователя и исследований - один к многим, значение таблицы в ForeignKey - всегда с маленькой 
         буквы '''
         user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
 
@@ -52,18 +57,50 @@ def main():
     @login_required
     def addTrials():
         if request.method == "POST":
-            username = (Users.query.filter_by(login=AUTHORIZED_LOGIN).first()).id
+            username = (Users.query.filter_by(login=AUTHORIZED_LOGIN).first()).login
             user_id = (Users.query.filter_by(login=AUTHORIZED_LOGIN).first()).id
             title = request.form['title']
-            trials = Trials(username=username, title=title, user_id=user_id)
+            number_of_participants = request.form['number_of_participants']
+            number_of_interventions = request.form['number_of_interventions']
+            trials = Trials(username=username, title=title,
+                            number_of_participants=number_of_participants,
+                            number_of_interventions=number_of_interventions, user_id=user_id)
             try:
                 db.session.add(trials)
                 db.session.commit()
-                return redirect('/trials')
+                #return redirect(url_for('addTrialsNext', number_of_interventions=number_of_interventions))
+                return render_template('index.html')
             except:
                 return "Ошибка при добавлении данных в БД"
         else:
-            return render_template('addTrials.html')
+            return render_template('addTrials.html', number_of_interventions=0)
+
+    @app.route('/addTrialsNext/<int:number_of_interventions>', methods=['GET', 'POST'])
+    @login_required
+    def addTrialsNext(number_of_interventions):
+        if request.method == "POST":
+            interventions = []  # список из вариантов вмешательств
+            for i in range(number_of_interventions):
+                interventions.append(request.form[f'{i}'])  # заполнение списка
+            '''Проверка элементов списка на уникальность'''
+            unique = True
+            for i in range(len(interventions) - 1):
+                for j in range(i + 1, len(interventions)):
+                    if interventions[i] == interventions[j]:
+                        unique = False
+                        break
+            if not unique:
+                flash('Поля совпадают')
+                return render_template("addTrialsNext.html", number_of_interventions=number_of_interventions)
+
+            # Рандомизация с помощью кода R, создание групп
+            r = robjects.r  # Определение сценария R и загрузка экземпляра в Python
+            r['source']('randomization.R')
+            randomization_function_r = robjects.globalenv['randomization']  # Загрузка функции, определенной в R.
+            data = randomization_function_r(15, number_of_interventions, interventions)
+            print(data)
+            return redirect('/trials')
+        return render_template("addTrialsNext.html", number_of_interventions=number_of_interventions)
 
     @app.route('/trials')
     def trials():
@@ -120,7 +157,7 @@ def main():
                 login_user(user)
                 global AUTHORIZED_LOGIN
                 AUTHORIZED_LOGIN = login
-                return redirect('/')
+                return redirect('/account')
             else:
                 flash('Ошибка авторизации')
         else:
@@ -138,7 +175,7 @@ def main():
     def account():
         user_query = Users.query.filter_by(login=AUTHORIZED_LOGIN).first()
         account_trials = Trials.query.filter_by(user_id=user_query.id).all()
-        return render_template("account.html", account_trials=account_trials)
+        return render_template("account.html", account_trials=account_trials, AUTHORIZED_LOGIN=AUTHORIZED_LOGIN)
 
     @app.route('/users')
     def users():
--- main.py
+++ main.py
@@ -2,11 +2,4 @@ import rpy2.robjects as robjects
 import app
 
 
-# Рандомизация с помощью кода R, создание групп
-r = robjects.r  # Определение сценария R и загрузка экземпляра в Python
-r['source']('randomization.R')
-randomization_function_r = robjects.globalenv['randomization']  # Загрузка функции, которую мы определили в R.
-data = randomization_function_r()
-# print(data)
-
 app.main()
